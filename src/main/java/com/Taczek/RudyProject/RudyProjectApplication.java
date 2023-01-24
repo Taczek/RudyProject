335a0276b9977f1c0231c21dd5f3e951de284a61
ptaczkow <pawel.taczkowski@unitygroup.com>
2023-01-24 09:09:24 +0100

    feat: project initialized from spring boot starter

---
+++ src/main/java/com/Taczek/RudyProject/RudyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Taczek.RudyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RudyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RudyProjectApplication.class, args);
+	}
+
+}
